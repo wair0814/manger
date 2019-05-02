10bc2ca964e5f3e059fd1a49ad8661afcc166e64
wair0814 <wair0814@163.com>
2019-05-02 17:18:07 +0800

    补齐sql文件

---
+++ src/test/java/cn/liwei08/server/impl/VideoApiServiceImplTest.java
@@ -0,0 +1,26 @@
+package cn.liwei08.server.impl;
+
+//import cn.liwei08.entity.VideoApi;
+//import cn.liwei08.mapper.VideoApiMapper;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class VideoApiServiceImplTest {
+    @Autowired
+//    VideoApiMapper videoApiMapper;
+
+    @Test
+    public void listVideoApi() {
+//        List<VideoApi> lists = videoApiMapper.listVideoApi();
+//        System.out.println(lists.size());
+    }
+}
